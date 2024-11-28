06e753aa9e263faf12bbc498281d29008c48f1c3
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-11-28 09:05:48 +0100

    Remove CommandGroup class and unused methods
    Refactored the `mod.ts` file to clean up unused code by removing the `CommandGroup` class along with its methods. This improves overall code maintainability and readability.

--- src/mod.ts
+++ src/mod.ts
@@ -1,21 +1,3 @@
 export {Command} from "./command.ts";
 export * from "./command-group.ts";
 export * from "./context.ts";
-
-class CommandGroup {
-    command() {
-        return this;
-    }
-
-    addToScope() {
-        return this;
-    }
-
-    setCommands() {
-        return this;
-    }
-}
-
-export const commands = () => () => ({})
-
-export {CommandGroup};
