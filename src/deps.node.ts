855822f3f5fd4c1c30758c51bd77ed572af9c866
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-11-28 08:58:10 +0100

    Add command management and localization features.
    Introduced a comprehensive command management system with class `CommandGroup` for grouping and handling commands, and `Command` for individual command logic. These include localizable command names and descriptions, scope-based registration, and methods for verification of compliance with Telegram Bot API standards.

---
+++ src/deps.node.ts
@@ -0,0 +1,25 @@
+export {
+    Api,
+    Bot,
+    type ChatTypeContext,
+    type ChatTypeMiddleware,
+    type CommandContext,
+    type CommandMiddleware,
+    Composer,
+    Context,
+    type Middleware,
+    type MiddlewareObj,
+    type NextFunction,
+} from "grammy";
+export type {
+    BotCommand,
+    BotCommandScope,
+    BotCommandScopeAllChatAdministrators,
+    BotCommandScopeAllGroupChats,
+    BotCommandScopeAllPrivateChats,
+    BotCommandScopeChat,
+    Chat,
+    LanguageCode,
+    MessageEntity
+} from "grammy/types";
+export { LanguageCodes } from "grammy/types";
